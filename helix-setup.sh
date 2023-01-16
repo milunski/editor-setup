dc172dd1dd0bdc0a64e7eacf545c3f03cc5dda81
milunski <matthewmilunski@gmail.com>
2023-01-16 00:10:17 -0600

    init commit

---
+++ helix-setup.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+
+
+# Download and install Helix
+setup_helix () {
+    if [ uname="Linux" ]
+    then
+        sudo apt update -y
+        sudo apt install cargo -y
+        cd ~
+        echo "export PATH=$HOME/.cargo/bin:$PATH" >> ~/.bashrc
+        git clone --recurse-submodules --shallow-submodules -j8 https://github.com/helix-editor/helix
+        source ~/.bashrc
+        cd helix
+        cargo install --locked --path helix-term
+    elif [ uname="Darwin" ]
+    then
+        brew install helix
+    fi
+    echo "Helix installed!"
+}
+
+setup_themes_languages () {
+    if [ ! -d ~/.config/helix/themes ]
+    then
+        mkdir ~/.config/helix/themes
+        echo "Themes directory created"
+    fi
+    if [ ! -d ~/.config/helix/languages ]
+    then
+        mkdir ~/.config/helix/languages
+        echo "Languages directory created"
+    fi
+}
+
+os=$( uname )
+
+if [ $os="Linux" ]
+then
+    echo "Setting up for Linux"
+    #setup_helix
+    setup_themes_languages
+else
+    echo "Nothing"
+fi
+echo "Setup complete. Restart the shell session and run hx --health to check if the install was performed correctly."
