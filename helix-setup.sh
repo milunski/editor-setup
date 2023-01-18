c7525f70cc5f1b2fd510a5ca6e65c48ee7024996
milunski <matthewmilunski@gmail.com>
2023-01-17 19:42:18 -0600

    os type detection

--- helix-setup.sh
+++ helix-setup.sh
@@ -3,47 +3,39 @@
 
 # Download and install Helix
 setup_helix () {
-    if [ uname="Linux" ]
-    then
+    if [[ "$OSTYPE" =~ ^linux ]]; then
+        echo "Installing for Linux"
         sudo apt update -y
         sudo apt install cargo -y
-        cd ~
-        echo "export PATH=$HOME/.cargo/bin:$PATH" >> ~/.bashrc
-        git clone --recurse-submodules --shallow-submodules -j8 https://github.com/helix-editor/helix
-        source ~/.bashrc
-        cd helix
-        cargo install --locked --path helix-term
-    elif [ uname="Darwin" ]
-    then
-        brew install helix
+    elif [[ "$OSTYPE" =~ ^darwin ]]; then
+        echo "Installing for Mac"
+        brew install rust 
     fi
+
+    cd ~
+    echo "export PATH=$HOME/.cargo/bin:$PATH" >> ~/.bashrc        
+    git clone --recurse-submodules --shallow-submodules -j8 https://github.com/helix-editor/helix
+    source ~/.bashrc
+    cd helix
+    cargo install --locked --path helix-term
     echo "Helix installed!"
 }
 
 setup_themes_languages () {
-    if [ ! -d ~/.config/helix/themes ]
-    then
+    if [[ ! -d ~/.config/helix/themes ]]; then
         mkdir ~/.config/helix/themes
         echo "Themes directory created"
         cp -r ~/helix/runtime/themes ~/.config/helix/
     fi
-    if [ ! -d ~/.config/helix/languages ]
-    then
+    if [[ ! -d ~/.config/helix/languages ]]; then
         mkdir ~/.config/helix/languages
         echo "Languages directory created"
         cp -r ~/helix/languages.toml ~/.config/helix/languages
     fi
 }
 
-os=$( uname )
 
-if [ $os="Linux" ]
-then
-    echo "Setting up for Linux"
-    setup_helix
-    cp ./config.toml ~/.config/helix/
-    setup_themes_languages
-else
-    echo "Nothing"
-fi
+setup_helix
+cp ./config.toml ~/.config/helix/
+setup_themes_languages
 echo "Setup complete. Restart the shell session and run hx --health to check if the install was performed correctly."
